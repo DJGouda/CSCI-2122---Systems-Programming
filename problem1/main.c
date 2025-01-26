0ddbb81a4c545689feae8fcf8e1197ee37b88d0d
duren <dr700813@dal.ca>
2025-01-26 17:12:06 -0400

    problem 1 done

--- problem1/main.c
+++ problem1/main.c
@@ -1,3 +1,32 @@
+/* @Duren Gouda
+ * B00949586
+ */
+
+#include <stdio.h>
+
 int main() {
+    int n;
+    scanf("%d", &n);
+
+    float totalSum = 0.0;
+
+    int amount;
+    char name[50];
+    float unitCost, totalCost;
+
+    for (int i = 0; i < n; i++) {
+
+        scanf("%d %s %f", &amount, name, &unitCost); // input from user
+
+        totalCost = amount * unitCost; // total cost of the product
+
+        totalSum += totalCost; // total bill(sum of all)
+
+        printf("%s : %.2f\n", name, totalCost);
+    }
+
+    printf("Total : %.2f\n", totalSum); // Print total
+
     return 0;
 }
+

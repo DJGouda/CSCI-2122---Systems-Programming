2293a4ef275f893f357155f0697a6f37f09640e6
duren <dr700813@dal.ca>
2025-01-26 17:36:53 -0400

    problem2 done

--- problem2/main.c
+++ problem2/main.c
@@ -1,4 +1,30 @@
+/* @Duren Gouda
+* B00949586
+ */
+
+#include <stdio.h>
+#include <string.h>
+
 int main() {
-    printf("Hello World\n");
+    char currentWord[29];
+    char previousWord[29] = "";
+
+    while (1) {
+        scanf("%s", currentWord);
+
+
+        if (strcmp(currentWord, ".") == 0) { // check for period "."
+            break;
+        }
+
+
+        if (strcmp(currentWord, previousWord) != 0) {  // Compares with previous word and print if different
+            printf("%s\n", currentWord);
+        }
+
+
+        strcpy(previousWord, currentWord); // Update the previous word
+    }
+
     return 0;
 }
